a55f943eeec0b90b00b455d333c6248a7b6166fa
jy.choi <jy.choi@yallafactory.com>
2021-04-09 09:29:59 +0900

    RecyclerView 학습

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Dictionary> mArrayList;
     private CustomAdapter mAdapter;
+    private int count = -1;
 
 
 
@@ -20,7 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView 
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager mLinearLaryoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLaryoutManager);
+
+        mArrayList = new ArrayList<>();
+
+        mAdapter = new CustomAdapter(mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                mLinearLaryoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        Button buttonInsert = (Button)findViewById(R.id.button);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+
+                Dictionary data = new Dictionary(count+"","Apple"+ count, "사과"+count);
+
+                mArrayList.add(data);
+
+                mAdapter.notifyDataSetChanged();
+            }
+        });
 
 
     }
