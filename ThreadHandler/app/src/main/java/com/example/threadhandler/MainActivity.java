a26cf79c28dddd7819d4717513c08cc0c97c2eae
jy.choi <cjy2103@naver.com>
2021-05-17 15:56:47 +0900

    Thread Handler

---
+++ ThreadHandler/app/src/main/java/com/example/threadhandler/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.threadhandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView clockTextView;
+    private static Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new Handler();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Calendar cal = Calendar.getInstance();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String strTime = sdf.format(cal.getTime());
+
+                clockTextView = findViewById(R.id.clock);
+                clockTextView.setText(strTime);
+            }
+        };
+
+        class NewRunnable implements Runnable {
+
+            @Override
+            public void run() {
+                while (true) {
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    mHandler.post(runnable);
+                }
+            }
+        }
+
+        NewRunnable nr = new NewRunnable();
+        Thread t = new Thread(nr);
+        t.start();
+
+    }
+}
