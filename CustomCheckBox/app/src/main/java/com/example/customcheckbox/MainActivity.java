d88fae9189a831bc4676fb370b56e355151e938c
jy.choi <cjy2103@naver.com>
2021-05-31 17:59:16 +0900

    dialog 체크박스 다시 알리지 않음 기능 구현 완료

--- CustomCheckBox/app/src/main/java/com/example/customcheckbox/MainActivity.java
+++ CustomCheckBox/app/src/main/java/com/example/customcheckbox/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.customcheckbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
@@ -10,11 +13,18 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn,saveNumber;
     Dialog dialog;
+    SharedPreferences sf;
+    CheckBox checkBox;
+    int tmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +32,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.button);
+        saveNumber = (Button) findViewById(R.id.saveNumber);
 
         dialog = new Dialog(MainActivity.this);
 
+        sf = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+        if(sf.contains("ischeck")){
+            tmp = sf.getInt("ischeck",0);
+        }
+
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.custom_dialog);
-//        WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
-//        params.width = WindowManager.LayoutParams.MATCH_PARENT;
-//        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
-//        dialog.getWindow().setAttributes(params);
 
 
         Display display = getWindowManager().getDefaultDisplay();
@@ -41,30 +54,74 @@ public class MainActivity extends AppCompatActivity {
 
         Window window = dialog.getWindow();
 
-        int x = (int)(size.x*0.8f);
-        int y = (int)(size.y*0.2f);
+        int x = (int)(size.x*0.9f);
+        int y = (int)(size.y*0.25f);
 
         window.setLayout(x,y);
-
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog();
+                if(tmp!=1) {
+                    showDialog();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"이미 체크박스가 선택되어 있어 더이상 뜨지 않음",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        saveNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"이미 체크박스가 선택되어있어 더이상 뜨지 않음 tmp 값 :"+tmp,Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        SharedPreferences.Editor edit = sf.edit();
+        //edit.putBoolean("ischeck",checkBox.isChecked());
+
+        // Boolean 안되서 Int로 지정... 마지막 tmp 값을 저장
+        edit.putInt("ischeck",tmp);
+
+        edit.commit();
+
+    }
+
     public void showDialog(){
 
+            dialog.show(); // 다이어로그 띄우기
 
-        dialog.show(); // 다이어로그 띄우기
 
-        dialog.findViewById(R.id.okButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
+
+            dialog.findViewById(R.id.okButton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            checkBox = dialog.findViewById(R.id.checkBox);
+
+            dialog.findViewById(R.id.checkBox).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(checkBox.isChecked()){
+                        Toast.makeText(MainActivity.this,"체크박스 선택",Toast.LENGTH_SHORT).show();
+                        tmp = 1;
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this,"체크박스 미선택",Toast.LENGTH_SHORT).show();
+                        tmp = 0;
+                    }
+                }
+            });
+
     }
 }
