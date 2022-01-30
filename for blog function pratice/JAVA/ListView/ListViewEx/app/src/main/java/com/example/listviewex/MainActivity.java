b29bd4d058a3a2b1ea463f0b4e0d7ed80b43f601
cjy2103 <cjy2103@naver.com>
2022-01-30 21:41:10 +0900

    listView 설명

---
+++ for blog function pratice/JAVA/ListView/ListViewEx/app/src/main/java/com/example/listviewex/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.listviewex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.listviewex.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initBinding();
+    }
+
+    /**
+     * @DESC: 초기 바인딩
+     */
+    private void initBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
