bd65bb21fbb04078d7b93d28bc2436215ec5fa95
cjy2103 <cjy2103@naver.com>
2022-01-31 12:38:07 +0900

    ListView 설명 완

--- for blog function pratice/JAVA/ListView/ListViewEx/app/src/main/java/com/example/listviewex/MainActivity.java
+++ for blog function pratice/JAVA/ListView/ListViewEx/app/src/main/java/com/example/listviewex/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.listviewex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.listviewex.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    List<String> list;
+    private Context mContext;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         initBinding();
+
+        initialize();
+        
+        listAdd();
+
+        listConnection();
+
+        clickItem();
     }
 
     /**
@@ -24,5 +41,51 @@ public class MainActivity extends AppCompatActivity {
     private void initBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    /**
+     * @DESC: 초기화
+     */
+    private void initialize(){
+        mContext = MainActivity.this;
+        list = new ArrayList<>();
+    }
+
+    /**
+     * @DESC: 리스트 뷰 아이템 추가
+     */
+    private void listAdd(){
+        list.add("사과");
+        list.add("바나나");
+        list.add("키위");
+        list.add("수박");
+        list.add("망고");
+        list.add("사과");
+        list.add("바나나");
+        list.add("키위");
+        list.add("수박");
+        list.add("망고");
+        list.add("사과");
+        list.add("바나나");
+        list.add("키위");
+        list.add("수박");
+        list.add("망고");
+    }
+
+    /**
+     * @DESC: List 어댑터에 연결
+     */
+    private void listConnection(){
+        adapter = new ArrayAdapter<>(mContext, android.R.layout.simple_list_item_1, list);
+        binding.listItem.setAdapter(adapter);
+    }
+
+    /**
+     * @DESC: 아이템 클릭
+     */
+    private void clickItem(){
+        binding.listItem.setOnItemClickListener((adapterView, view, position, l) -> {
+            binding.tvSelect.setText((String) adapterView.getItemAtPosition(position));
+        });
     }
 }
