a9096b5b557cc7c3d9d0c1af8993d12b601af808
jy.choi <cjy2103@naver.com>
2022-02-10 15:32:46 +0900

    runOnUiThread Timer 완

--- for blog function pratice/JAVA/Thread/runOnUiThread/app/src/main/java/com/example/runonuithreadclock/MainActivity.java
+++ for blog function pratice/JAVA/Thread/runOnUiThread/app/src/main/java/com/example/runonuithreadclock/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.runonuithreadclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.runonuithreadclock.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         initBinding();
 
-        
+        timerSetting();
+
     }
 
     /**
@@ -26,5 +33,38 @@ public class MainActivity extends AppCompatActivity {
     private void initBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    private void timerSetting(){
+        runnable = () -> {
+            Calendar calendar = Calendar.getInstance();
+
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat
+                    = new SimpleDateFormat("HH:mm:ss");
+            String strTime = simpleDateFormat.format(calendar.getTime());
+
+            binding.tvTime.setText(strTime);
+        };
+
+        NewRunnable newRunnable = new NewRunnable() ;
+        Thread thread = new Thread(newRunnable);
+        thread.start();
+    }
+
+    class NewRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace() ;
+                }
+
+                // 메인 스레드에 runnable 전달.
+                runOnUiThread(runnable) ;
+            }
+        }
     }
 }
