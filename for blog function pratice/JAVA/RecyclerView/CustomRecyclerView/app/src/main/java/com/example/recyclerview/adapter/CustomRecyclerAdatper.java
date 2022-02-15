a6405f2170284feafbb810e2c4f4d0ec64c5beec
jy.choi <cjy2103@naver.com>
2022-02-15 17:57:06 +0900

    CustomRecyclerView 작성중

---
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerView/app/src/main/java/com/example/recyclerview/adapter/CustomRecyclerAdatper.java
@@ -0,0 +1,42 @@
+package com.example.recyclerview.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerview.databinding.RecyclerViewListBinding;
+
+public class CustomRecyclerAdatper extends RecyclerView.Adapter<CustomRecyclerAdatper.ViewHolder>{
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        RecyclerViewListBinding binding;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            initBinding();
+        }
+
+        private void initBinding(){
+            binding = RecyclerViewListBinding.bind(itemView);
+        }
+    }
+}
