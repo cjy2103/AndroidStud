89a900019012d0abded9cfb1ec30b54ad1516dc0
jy.choi <cjy2103@naver.com>
2022-02-16 17:46:42 +0900

    RecyclerView 완

--- for blog function pratice/JAVA/RecyclerView/CustomRecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,16 +1,77 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.recyclerview.adapter.CustomRecyclerAdatper;
+import com.example.recyclerview.databinding.ActivityMainBinding;
+import com.example.recyclerview.model.ListItemModel;
+import com.example.recyclerview.model.MyListItem;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ArrayList<MyListItem> myListItems;
+    private CustomRecyclerAdatper adatper;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initBinding();
 
+        initialize();
+
+        listadd();
+
+        adatper = new CustomRecyclerAdatper(this,this,myListItems);
+        binding.recyclerList.setAdapter(adatper);
+
+
+    }
+
+    private void initBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void initialize(){
+        myListItems = new ArrayList<>();
+        binding.recyclerList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void listadd(){
+        String imageUri = "drawable://";
+        addItem(getResources().getString(R.string.baknana),getResources().getString(R.string.bak_describe),imageUri + R.drawable.baknana);
+        addItem(getResources().getString(R.string.djmax),getResources().getString(R.string.djmax_describe),imageUri + R.drawable.djmax_clear_fail);
+        addItem(getResources().getString(R.string.djmax_falling_love),getResources().getString(R.string.djmax_falling_love_describe),imageUri + R.drawable.djmax_falling_in_love);
+        addItem(getResources().getString(R.string.mwamwa),getResources().getString(R.string.mwamwa_describe),imageUri + R.drawable.mwama);
+        addItem(getResources().getString(R.string.tamtam),getResources().getString(R.string.mwamwa_describe),imageUri + R.drawable.tamtam);
+    }
+
+    private void addItem(String title, String describe, String uri){
+        ListItemModel listItemModel = new ListItemModel();
+        listItemModel.setTitle(title);
+        listItemModel.setDescribe(describe);
+        listItemModel.setUri(uri);
+
+        MyListItem myListItem = new MyListItem();
+
+        ArrayList<ListItemModel> items = new ArrayList<>();
+
+        items.add(listItemModel);
+
+        myListItem.setList(items);
+
+        myListItems.add(myListItem);
     }
 }
--- for blog function pratice/JAVA/RecyclerView/CustomRecyclerView/app/src/main/java/com/example/recyclerview/adapter/CustomRecyclerAdatper.java
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerView/app/src/main/java/com/example/recyclerview/adapter/CustomRecyclerAdatper.java
@@ -1,32 +1,56 @@
 package com.example.recyclerview.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.recyclerview.R;
 import com.example.recyclerview.databinding.RecyclerViewListBinding;
+import com.example.recyclerview.model.MyListItem;
+
+import java.util.ArrayList;
 
 public class CustomRecyclerAdatper extends RecyclerView.Adapter<CustomRecyclerAdatper.ViewHolder>{
+
+    private Context context;
+    private Activity activity;
+    private ArrayList<MyListItem> myListItem;
+
+    public CustomRecyclerAdatper(Context context, Activity activity, ArrayList<MyListItem> myListItem) {
+        this.context = context;
+        this.activity = activity;
+        this.myListItem = myListItem;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_view_list, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Uri imagePath = Uri.parse("android.resource://" + activity.getPackageName() + "/" + myListItem.get(position).getList().get(0).getUri());
+        Glide.with(context).load(imagePath).into(holder.binding.ivArt);
+        holder.binding.tvTitle.setText(myListItem.get(position).getList().get(0).getTitle());
+        holder.binding.tvDescribe.setText(myListItem.get(position).getList().get(0).getDescribe());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return myListItem.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder{
         RecyclerViewListBinding binding;
 
         public ViewHolder(@NonNull View itemView) {
