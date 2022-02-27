9134ba7d3be0f1f8bb95445de2be54368a349583
cjy2103 <cjy2103@naver.com>
2022-02-28 02:20:07 +0900

    NumberPicker 가로모드 설명중

---
+++ for blog function pratice/JAVA/etc/NumberPicker(horizontal)/app/src/main/java/com/example/numberpickehorizontal/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.numberpickehorizontal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.numberpickehorizontal.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initBinding();
+
+
+    }
+
+    /**
+     * @DESC: 초기 바인딩
+     */
+    private void initBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
