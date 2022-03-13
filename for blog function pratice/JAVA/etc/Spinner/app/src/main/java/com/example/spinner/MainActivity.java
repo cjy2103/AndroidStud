5eb779fde7b30fb198c2755f1dc56f7c8bfaf60b
cjy2103 <cjy2103@naver.com>
2022-03-14 01:32:20 +0900

    Spinner 완

---
+++ for blog function pratice/JAVA/etc/Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.example.spinner.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private String[] spinnerList;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initBinding();
+
+        initialize();
+
+        setAdpater();
+
+        spinnerSelect();
+    }
+
+    private void initBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @SuppressLint("ResourceType")
+    private void initialize(){
+        spinnerList = getResources().getStringArray(R.array.spin_item);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, spinnerList);
+    }
+
+    private void setAdpater(){
+        binding.spinner.setAdapter(adapter);
+    }
+
+    private void spinnerSelect(){
+        binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                binding.tvSelect.setText(spinnerList[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
--- for blog function pratice/JAVA/etc/Spinner/app/src/test/java/com/example/spinner/ExampleUnitTest.java
+++ for blog function pratice/JAVA/etc/Spinner/app/src/test/java/com/example/spinner/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.spinner;
 
 import org.junit.Test;
 
