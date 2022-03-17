e1219b644e275e4a14b150a78fa6011436b56359
jy.choi <cjy2103@naver.com>
2022-03-17 12:02:16 +0900

    Network 상태체크 포스팅 완

--- for blog function pratice/JAVA/System/WifiMobileConeectCheck/app/src/main/java/com/example/wifimobilecheck/MainActivity.java
+++ for blog function pratice/JAVA/System/WifiMobileConeectCheck/app/src/main/java/com/example/wifimobilecheck/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.wifimobilecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.NetworkRequest;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.wifimobilecheck.databinding.ActivityMainBinding;
+import com.example.wifimobilecheck.network.NetWorkStatus;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +22,28 @@ public class MainActivity extends AppCompatActivity {
         stateCheck();
     }
 
+    /**
+     * @DESC: 뷰바인딩
+     */
     private void initBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 
+    /**
+     * @DESC: 네트워크 상태체크
+     */
+    @SuppressLint("SetTextI18n")
     private void stateCheck(){
         binding.btnState.setOnClickListener(v->{
-
+            int netWorkState = NetWorkStatus.getConnectStatus(this);
+            if(netWorkState == NetWorkStatus.TYPE_WIFI){
+                binding.tvState.setText("상태 : WIFI");
+            } else if(netWorkState == NetWorkStatus.TYPE_MOBILE){
+                binding.tvState.setText("상태 : MOBILE");
+            } else {
+                binding.tvState.setText("상태 : 끊김");
+            }
         });
     }
 
--- for blog function pratice/JAVA/System/WifiMobileConeectCheck/app/src/main/java/com/example/wifimobilecheck/network/NetWorkStatus.java
+++ for blog function pratice/JAVA/System/WifiMobileConeectCheck/app/src/main/java/com/example/wifimobilecheck/network/NetWorkStatus.java
@@ -1,15 +1,12 @@
-package com.example.wifimobilecheck;
+package com.example.wifimobilecheck.network;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
-import android.net.NetworkRequest;
 import android.os.Build;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 
 public class NetWorkStatus {
     public static final int TYPE_WIFI = 1;
@@ -18,13 +15,12 @@ public class NetWorkStatus {
 
 
     /**
-     * @DESC: API 29 이전에 사용했던 기존방식
+     * @DESC: 기존에 있던 방식 (Deprecated 됨)
      * @param context
      * @return
      */
     public static int getConnectStatusAsIs(Context context){
         ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
         if(networkInfo != null){
             int type = networkInfo.getType();
@@ -37,15 +33,35 @@ public class NetWorkStatus {
         return TYPE_NOT_CONNECTED;  //연결이 되지않은 상태
     }
 
+    /**
+     * @DESC :네트워크 상태
+     * @param context
+     * @return
+     */
     public static int getConnectStatus(Context context){
         ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        Network network = manager.getActiveNetwork();
-        if(network != null){
-            NetworkCapabilities networkCapabilities  = manager.getNetworkCapabilities(network);
-            if(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)){
-                return TYPE_WIFI;
-            } else if(networkCapabilities.hasTransport(NetworkCapabilities.))
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Network network = manager.getActiveNetwork();
+            if(network != null){
+                NetworkCapabilities networkCapabilities  = manager.getNetworkCapabilities(network);
+                if(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)){
+                    return TYPE_WIFI;
+                } else if(networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)){
+                    return TYPE_MOBILE;
+                }
+            }
+        } else {
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            if(networkInfo != null){
+                int type = networkInfo.getType();
+                if(type == ConnectivityManager.TYPE_MOBILE){
+                    return TYPE_MOBILE;
+                }else if(type == ConnectivityManager.TYPE_WIFI){
+                    return TYPE_WIFI;
+                }
+            }
         }
+        return TYPE_NOT_CONNECTED;
     }
 
 }
