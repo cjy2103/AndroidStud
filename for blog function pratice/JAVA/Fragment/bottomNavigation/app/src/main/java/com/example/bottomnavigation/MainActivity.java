c04851bbe76477c6de78d75fc4079b1c548409b8
jy.choi <cjy2103@naver.com>
2022-03-30 16:20:01 +0900

    Bottom Navigation 작업중

--- for blog function pratice/JAVA/Fragment/bottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ for blog function pratice/JAVA/Fragment/bottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.bottomnavigation.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewBinding();
+
+        initialize();
+    }
+
+    /**
+     * @DESC: 뷰바인딩
+     */
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void initialize(){
+        binding.bottomNav.setItemIconTintList(null);
     }
 }
