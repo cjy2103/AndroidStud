3d39114722828bbf17be3d4e9b4d96e42038f541
jy.choi <cjy2103@naver.com>
2022-03-30 18:00:43 +0900

    블로그 포스팅 하면 됨

---
+++ for blog function pratice/JAVA/Fragment/bottomNavigation/app/src/main/java/com/example/bottomnavigation/activity/main/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.bottomnavigation.activity.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.bottomnavigation.R;
+import com.example.bottomnavigation.databinding.ActivityMainBinding;
+import com.example.bottomnavigation.fragment.DjMaxFragment;
+import com.example.bottomnavigation.fragment.MidoriFragment;
+import com.example.bottomnavigation.fragment.MomoiFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Menu menu;
+    private Fragment djMaxFragment, momoiFramgnet
+            , midoriFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewBinding();
+
+        initialize();
+
+        bottomNavigationSelect();
+    }
+
+    /**
+     * @DESC: 뷰바인딩
+     */
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void initialize(){
+        binding.bottomNav.setItemIconTintList(null);
+
+        menu = binding.bottomNav.getMenu();
+
+        djMaxFragment = new DjMaxFragment();
+        // 첫화면
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, djMaxFragment).commit();
+
+    }
+
+    /**
+     * @DESC: 바텀 네비바 메뉴 선택
+     */
+    private void bottomNavigationSelect(){
+        binding.bottomNav.setOnItemSelectedListener(item->{
+            changeFramgnet(item);
+            return true;
+        });
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void changeFramgnet(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.djmax_fragment:
+                if(djMaxFragment == null){
+                    djMaxFragment = new DjMaxFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, djMaxFragment).commit();
+                break;
+            case R.id.momoi_fragment:
+                if(momoiFramgnet == null){
+                    momoiFramgnet = new MomoiFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, momoiFramgnet).commit();
+                break;
+            case R.id.midori_fragment:
+                if(midoriFragment == null){
+                    midoriFragment = new MidoriFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, midoriFragment).commit();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
