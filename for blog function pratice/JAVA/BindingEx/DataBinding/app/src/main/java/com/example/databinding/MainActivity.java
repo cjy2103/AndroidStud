2f80cc21bc21e17cd05552f26fab4e3942fbc0a6
cjy2103 <cjy2103@naver.com>
2022-04-04 21:58:41 +0900

    DataBinding 단순 Layout일때

--- for blog function pratice/JAVA/BindingEx/DataBinding/app/src/main/java/com/example/databinding/MainActivity.java
+++ for blog function pratice/JAVA/BindingEx/DataBinding/app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dataBinding();
 
+        clickBtn();
+    }
+
+    /**
+     * @DESC: dataBinding
+     */
+    private void dataBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    /**
+     * @DESC: 버튼 클릭
+     */
+    private void clickBtn(){
+        binding.btnClick.setOnClickListener(v->{
+            Toast.makeText(this, "버튼 클릭됨",Toast.LENGTH_LONG).show();
+        });
     }
 
 
