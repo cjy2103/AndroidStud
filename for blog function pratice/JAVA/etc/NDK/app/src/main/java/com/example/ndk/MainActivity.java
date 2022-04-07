6607b9bed9da768fe8b2d87245359158f7d37a26
cjy2103 <cjy2103@naver.com>
2022-04-07 18:31:21 +0900

    NDK 블로그 포스팅 완

--- for blog function pratice/JAVA/etc/NDK/app/src/main/java/com/example/ndk/MainActivity.java
+++ for blog function pratice/JAVA/etc/NDK/app/src/main/java/com/example/ndk/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.ndk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.ndk.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        try{
+            System.loadLibrary("test");
+        }
+        catch (UnsatisfiedLinkError ule){
+            Log.v("NDKTest","NDK 오류발생");
+            ule.printStackTrace();
+        }
+    }
+
+    private ActivityMainBinding binding;
+    private native String getNdk();
+
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewBinding();
+        initialize();
+        clickNdkTest();
+    }
+
+    /**
+     * @DESC: 뷰바인딩
+     */
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    /**
+     * @DESC: 초기화
+     */
+    private void initialize(){
+        str = getNdk();
+    }
+
+    /**
+     * @DESC: NDK 테스트 버튼 클릭
+     */
+    private void clickNdkTest(){
+        binding.btnClick.setOnClickListener(v->{
+            binding.tvNdk.setText(str);
+        });
     }
 }
