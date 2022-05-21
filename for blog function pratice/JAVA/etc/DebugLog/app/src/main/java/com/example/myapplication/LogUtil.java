2aab1b18e1b0b6d5db0e92a5f04e5a00a9057d2c
cjy2103 <cjy2103@naver.com>
2022-05-21 23:15:49 +0900

    디버그용 로그 포스팅 완

---
+++ for blog function pratice/JAVA/etc/DebugLog/app/src/main/java/com/example/myapplication/LogUtil.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.util.Log;
+
+public class LogUtil {
+    public static void log(String x){
+        if(BuildConfig.DEBUG)
+            Log.i("LogUtil",x);
+    }
+
+    public static void log(int x){
+        if(BuildConfig.DEBUG)
+            Log.i("LogUtil",String.valueOf(x));
+    }
+
+    public static void log(String tag, String x){
+        if(BuildConfig.DEBUG)
+            Log.i(tag, x);
+    }
+
+    public static void log(String tag, int x){
+        if(BuildConfig.DEBUG)
+            Log.i(tag, String.valueOf(x));
+    }
+}
