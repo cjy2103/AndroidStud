54ea010400bb3c74ae0e98b7e8d23917fe5d2479
jy.choi <cjy2103@naver.com>
2022-05-23 18:04:56 +0900

    구글 드라이브 API 모듈

---
+++ for company function practice/Test/GoogleDriveAPITest/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.myapplication;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.util.Collections;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "테스트";
+
+    private ActivityMainBinding binding;
+
+    private ActivityResultLauncher<Intent> resultLauncher;
+
+    private ActivityResultLauncher<Intent> signResultLauncher;
+
+    private DriveServiceHelper mDriveServiceHelper;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding();
+
+        callback();
+
+        signIn();
+
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void callback(){
+        signResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult() , result -> {
+                    handleSignInResult(result.getData());
+                }
+        );
+
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result ->{
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        if(result.getData() == null){
+                            return;
+                        }
+
+                        Uri uri = result.getData().getData();
+                        if (uri != null) {
+
+                            openFileFromFilePicker(uri);
+                        }
+                    }
+                }
+        );
+    }
+
+    private void signIn() {
+        binding.button.setOnClickListener(v->{
+            GoogleSignInOptions signInOptions =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                            .build();
+            GoogleSignInClient client = GoogleSignIn.getClient(this, signInOptions);
+
+            signResultLauncher.launch(client.getSignInIntent());
+        });
+
+
+    }
+
+    private void handleSignInResult(Intent result) {
+        GoogleSignIn.getSignedInAccountFromIntent(result)
+                .addOnSuccessListener(googleAccount -> {
+                    Log.v(TAG,"여기탐");
+                    // Use the authenticated account to sign in to the Drive service.
+                    GoogleAccountCredential credential =
+                            GoogleAccountCredential.usingOAuth2(
+                                    this, Collections.singleton(DriveScopes.DRIVE_FILE));
+                    credential.setSelectedAccount(googleAccount.getAccount());
+                    Drive googleDriveService =
+                            new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("Drive API Migration")
+                                    .build();
+
+                    // The DriveServiceHelper encapsulates all REST API and SAF functionality.
+                    // Its instantiation is required before handling any onClick actions.
+                    mDriveServiceHelper = new DriveServiceHelper(googleDriveService);
+                    openFilePicker();
+//                    createFile();
+                })
+                .addOnFailureListener(exception -> Log.e(TAG, "Unable to sign in.", exception));
+    }
+
+    private void openFilePicker() {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Opening file picker.");
+
+            Intent pickerIntent = mDriveServiceHelper.createFilePickerIntent();
+
+            // The result of the SAF Intent is handled in onActivityResult.
+            resultLauncher.launch(pickerIntent);
+        }
+    }
+
+    private void openFileFromFilePicker(Uri uri) {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Opening " + uri.getPath());
+
+            Log.d(TAG, "Opening " + uri.getPath());
+
+
+            mDriveServiceHelper.openFileUsingStorageAccessFramework(getContentResolver(), uri)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        mDriveService.files()
+                        Log.v(TAG,"여기서 작업?"+name+".....//!!!!"+content);
+//                        createFile();
+//                        mFileTitleEditText.setText(name);
+//                        mDocContentEditText.setText(content);
+
+                        // Files opened through SAF cannot be modified.
+//                        setReadOnlyMode();
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e(TAG, "Unable to open file from picker.", exception));
+        }
+    }
+
+    private void createFile() {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Creating a file.");
+
+            mDriveServiceHelper.createFile()
+                    .addOnSuccessListener(this::readFile)
+                    .addOnFailureListener(exception ->
+                            Log.e(TAG, "Couldn't create file.", exception));
+        }
+    }
+
+    private void readFile(String fileId) {
+        if (mDriveServiceHelper != null) {
+            Log.d(TAG, "Reading file " + fileId);
+
+            mDriveServiceHelper.readFile(fileId)
+                    .addOnSuccessListener(nameAndContent -> {
+                        String name = nameAndContent.first;
+                        String content = nameAndContent.second;
+
+                        Log.v(TAG,"여기서 작업?"+name+".....//!!!!"+content+"Id:"+fileId);
+//                        mFileTitleEditText.setText(name);
+//                        mDocContentEditText.setText(content);
+
+//                        setReadWriteMode(fileId);
+                    })
+                    .addOnFailureListener(exception ->
+                            Log.e(TAG, "Couldn't read file.", exception));
+        }
+    }
+
+
+}
