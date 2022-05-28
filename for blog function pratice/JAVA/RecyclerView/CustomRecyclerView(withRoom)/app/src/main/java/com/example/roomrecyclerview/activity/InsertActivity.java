6494ea8595e9b10305fdecc1626abbcb14d831bb
cjy2103 <cjy2103@naver.com>
2022-05-28 10:35:27 +0900

    Activity Dialog 양방향 값 전달 포스팅 완

--- for blog function pratice/JAVA/RecyclerView/CustomRecyclerView(withRoom)/app/src/main/java/com/example/roomrecyclerview/activity/InsertActivity.java
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerView(withRoom)/app/src/main/java/com/example/roomrecyclerview/activity/InsertActivity.java
@@ -1,15 +1,23 @@
 package com.example.roomrecyclerview.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.roomrecyclerview.R;
+import com.example.roomrecyclerview.activity.dialog.ImageSelectDialog;
 import com.example.roomrecyclerview.databinding.ActivityInsertBinding;
+import com.example.roomrecyclerview.util.LogUtil;
 
 public class InsertActivity extends AppCompatActivity {
 
     private ActivityInsertBinding binding;
+
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,8 @@ public class InsertActivity extends AppCompatActivity {
         viewBinding();
 
         clickInsert();
+
+        context = this;
 
     }
 
@@ -27,7 +37,17 @@ public class InsertActivity extends AppCompatActivity {
 
     private void clickInsert(){
         binding.btnInsert.setOnClickListener(v->{
+            FragmentManager fragmentManager = getSupportFragmentManager();
 
+            DialogFragment dialogFragment = new ImageSelectDialog();
+            Bundle bundle = new Bundle();
+            bundle.putString("Test","테스트");
+            dialogFragment.setArguments(bundle);
+            dialogFragment.show(fragmentManager,"Dialog");
         });
+    }
+
+    public void test(){
+        LogUtil.log("테스트");
     }
 }
--- for blog function pratice/JAVA/RecyclerView/CustomRecyclerView(withRoom)/app/src/main/java/com/example/roomrecyclerview/activity/dialog/ImageSelectDialog.java
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerView(withRoom)/app/src/main/java/com/example/roomrecyclerview/activity/dialog/ImageSelectDialog.java
@@ -1,5 +1,6 @@
 package com.example.roomrecyclerview.activity.dialog;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,24 +13,36 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.roomrecyclerview.R;
+import com.example.roomrecyclerview.activity.InsertActivity;
 import com.example.roomrecyclerview.activity.LocalImageSelectActivity;
 import com.example.roomrecyclerview.databinding.DialogImageSelectBinding;
+import com.example.roomrecyclerview.util.LogUtil;
 
 public class ImageSelectDialog extends DialogFragment {
 
     private DialogImageSelectBinding binding;
     private Context mContext;
+    private Activity mActivity;
+    private InsertActivity insertActivity;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         mContext = context;
+        mActivity = (Activity) context;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mContext = null;
+        mActivity = null;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        insertActivity.test();
     }
 
     @Nullable
@@ -47,9 +60,21 @@ public class ImageSelectDialog extends DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dataLoad();
+
         clickClose();
 
         clickLocal();
+
+
+        insertActivity = (InsertActivity)mActivity;
+
+    }
+
+    private void dataLoad(){
+        assert getArguments() != null;
+        String test = getArguments().getString("Test");
+        LogUtil.log("값있나?"+test);
     }
 
     private void clickClose(){
@@ -57,6 +82,7 @@ public class ImageSelectDialog extends DialogFragment {
             dismiss();
         });
     }
+
 
     private void clickLocal(){
         binding.btnLocal.setOnClickListener(v->{
