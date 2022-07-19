bfcd40160868fcef32802df750129096fccebb62
cjy2103 <cjy2103@naver.com>
2022-07-20 03:05:59 +0900

    액티비티 창 모두 닫기 포스팅중

--- for blog function pratice/JAVA/etc/ActivityAllClose/app/src/main/java/com/example/activityallclose/MainActivity.java
+++ for blog function pratice/JAVA/etc/ActivityAllClose/app/src/main/java/com/example/activityallclose/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.activityallclose;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.activityallclose.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
+
+        openActivity();
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void openActivity(){
+        binding.btnOpen.setOnClickListener(v->{
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("number",1);
+            startActivity(intent);
+        });
     }
 }
---
+++ for blog function pratice/JAVA/etc/ActivityAllClose/app/src/main/java/com/example/activityallclose/SubActivity.java
@@ -0,0 +1,55 @@
+package com.example.activityallclose;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.activityallclose.databinding.ActivitySubBinding;
+
+public class SubActivity extends AppCompatActivity {
+
+    private ActivitySubBinding binding;
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding();
+
+        initialize();
+
+        openActivity();
+        closeActivity();
+        closeAllActivity();
+    }
+
+    private void viewBinding(){
+        binding = ActivitySubBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void initialize(){
+        Intent intent = getIntent();
+        number = intent.getIntExtra("number",1);
+        binding.tvCurrent.setText("현재"+number+"째 창");
+    }
+
+    private void openActivity(){
+        binding.btnOpen.setOnClickListener(v->{
+            Intent intent = new Intent(this, SubActivity.class);
+            intent.putExtra("number",number+1);
+            startActivity(intent);
+        });
+    }
+
+    private void closeActivity(){
+
+    }
+
+    private void closeAllActivity(){
+
+    }
+}
