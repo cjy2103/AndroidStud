e4a20c9cc1bc21eee7098d075c7680fdd42f1b3b
jy.choi <cjy2103@naver.com>
2022-08-12 16:48:35 +0900

    구독 취소

--- for company function practice/SubScribeCancel/app/src/main/java/com/example/myapplication/MainActivity.java
+++ for company function practice/SubScribeCancel/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
@@ -14,10 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
+
+        btnAppSubscribe();
+        btnAllSubscribe();
     }
 
     private void viewBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    private void btnAppSubscribe(){
+        binding.btnAppSubscribe.setOnClickListener(v->{
+            Uri uri = Uri.parse("https://play.google.com/store/account/subscriptions?sku=your-sub-product-id&package=your-app-package");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.setPackage("com.android.vending");
+            startActivity(intent);
+        });
+    }
+
+    private void btnAllSubscribe(){
+        binding.btnAllSubscribe.setOnClickListener(v->{
+            Uri uri = Uri.parse("https://play.google.com/store/account/subscriptions");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.setPackage("com.android.vending");
+            startActivity(intent);
+        });
     }
 }
