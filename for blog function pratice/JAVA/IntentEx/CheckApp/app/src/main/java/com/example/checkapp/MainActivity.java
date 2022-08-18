9aeaaf0804adddf19fb58056729ee8d140cc68e7
jy.choi <cjy2103@naver.com>
2022-08-18 14:31:04 +0900

    다른앱 접근 포스팅중

--- for blog function pratice/JAVA/IntentEx/CheckApp/app/src/main/java/com/example/checkapp/MainActivity.java
+++ for blog function pratice/JAVA/IntentEx/CheckApp/app/src/main/java/com/example/checkapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.checkapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.checkapp.databinding.ActivityMainBinding;
 
@@ -14,10 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
+
+        btnUmaCheck();
+
+        btnPokemonCheck();
     }
 
     private void viewBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    private void btnUmaCheck(){
+        binding.btnUmaCheck.setOnClickListener(v->{
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.kakaogames.umamusume");
+            if(intent != null){
+                Toast.makeText(this, "우마무스메 있음", Toast.LENGTH_SHORT).show();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "우마무스메 없음", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void btnPokemonCheck(){
+        binding.btnPokemonCheck.setOnClickListener(v->{
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.nianticlabs.pokemongo");
+            if(intent != null){
+                Toast.makeText(this, "포켓몬고 있음", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "포켓몬고 없음", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
