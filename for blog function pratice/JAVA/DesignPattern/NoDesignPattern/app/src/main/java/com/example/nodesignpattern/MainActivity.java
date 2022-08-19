f3373a090837339d2d7686dd55e51c85bc383e80
cjy2103 <cjy2103@naver.com>
2022-08-19 23:32:09 +0900

    아키텍쳐 적용전 코드

--- for blog function pratice/JAVA/DesignPattern/NoDesignPattern/app/src/main/java/com/example/nodesignpattern/MainActivity.java
+++ for blog function pratice/JAVA/DesignPattern/NoDesignPattern/app/src/main/java/com/example/nodesignpattern/MainActivity.java
@@ -3,25 +3,43 @@ package com.example.nodesignpattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.nodesignpattern.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private ArrayList<Integer> arrayList;
+
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
 
-        binding.btnPick.setOnClickListener(v->{
+        initialize();
 
+        binding.btnPick.setOnClickListener(v->{
+            int select = random.nextInt(arrayList.size());
+            binding.imageView.setImageResource(arrayList.get(select));
         });
     }
 
     private void viewBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    private void initialize(){
+        random = new Random();
+        arrayList = new ArrayList<>(Arrays.asList(R.drawable.baknana,R.drawable.djmax_clear_fail,R.drawable.djmax_falling_in_love
+                ,R.drawable.mwama, R.drawable.tamtam));
     }
 }
