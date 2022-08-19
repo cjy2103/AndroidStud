1416dee7cc77c4a13f04de4dd4ac0fd254669534
jy.choi <cjy2103@naver.com>
2022-08-19 18:01:42 +0900

    노 아키텍처

--- for blog function pratice/JAVA/DesignPattern/NoDesignPattern/app/src/main/java/com/example/nodesignpattern/MainActivity.java
+++ for blog function pratice/JAVA/DesignPattern/NoDesignPattern/app/src/main/java/com/example/nodesignpattern/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
+
+        binding.btnPick.setOnClickListener(v->{
+
+        });
     }
 
     private void viewBinding(){
