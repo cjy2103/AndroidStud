aff0a2f9ab55bed118a49df2aeb5af923e9a5b8d
cjy2103 <cjy2103@naver.com>
2022-09-29 03:43:46 +0900

    Drawer Navigation 토스트 메시지

--- for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.drawernavigation.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private DrawerLayout drawer;
     private NavigationView navigationView;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         drawer = binding.drawerLayout;
         navigationView = binding.navView;
 
+        context = this;
+
+        binding.navView.setItemIconTintList(null);
     }
 
     private void openDrawer(){
@@ -47,13 +55,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("NonConstantResourceId")
     private void navigationItemClick(){
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                
-                return false;
+        navigationView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.nav_profile :
+                    Toast.makeText(context, "프로필 클릭", Toast.LENGTH_SHORT).show();
+                    drawer.closeDrawer(GravityCompat.START);
+                    break;
+                case R.id.nav_naver:
+                    Toast.makeText(context, "네이버 클릭", Toast.LENGTH_SHORT).show();
+                    drawer.closeDrawer(GravityCompat.START);
+                    break;
+                case R.id.nav_github:
+                    Toast.makeText(context, "GitHub 클릭", Toast.LENGTH_SHORT).show();
+                    drawer.closeDrawer(GravityCompat.START);
             }
+            return false;
         });
     }
 }
