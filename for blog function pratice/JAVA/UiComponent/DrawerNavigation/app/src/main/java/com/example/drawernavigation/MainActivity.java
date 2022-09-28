65bdcbc0a8c41ef10c7548f5e5406adbc6a0fe5d
jy.choi <cjy2103@naver.com>
2022-09-28 17:58:28 +0900

    UI 거의 완성

--- for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.drawernavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.example.drawernavigation.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
+
+        init();
+
+        openDrawer();
+
+        navigationItemClick();
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void init(){
+        drawer = binding.drawerLayout;
+        navigationView = binding.navView;
+
+    }
+
+    private void openDrawer(){
+        binding.btnOpen.setOnClickListener(v->{
+            drawer.openDrawer(GravityCompat.START);
+        });
+    }
+
+    private void navigationItemClick(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                
+                return false;
+            }
+        });
     }
 }
