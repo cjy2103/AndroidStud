03d1ba59ad244a3c83facfb1fab15dd2db01ed49
jy.choi <cjy2103@naver.com>
2022-09-29 17:10:28 +0900

    Activity, AppCompatActivity 차이 포스팅 완

--- for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/DrawerNavigation/app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.drawernavigation;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,9 +7,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.drawernavigation.databinding.ActivityMainBinding;
@@ -19,8 +15,6 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private DrawerLayout drawer;
-    private NavigationView navigationView;
     private Context context;
 
     @Override
@@ -41,9 +35,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
-        drawer = binding.drawerLayout;
-        navigationView = binding.navView;
-
         context = this;
 
         binding.navView.setItemIconTintList(null);
@@ -51,25 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void openDrawer(){
         binding.btnOpen.setOnClickListener(v->{
-            drawer.openDrawer(GravityCompat.START);
+            binding.drawerLayout.openDrawer(GravityCompat.START);
         });
     }
 
     @SuppressLint("NonConstantResourceId")
     private void navigationItemClick(){
-        navigationView.setNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.nav_profile :
+        binding.navView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_profile:
                     Toast.makeText(context, "프로필 클릭", Toast.LENGTH_SHORT).show();
-                    drawer.closeDrawer(GravityCompat.START);
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
                     break;
                 case R.id.nav_naver:
                     Toast.makeText(context, "네이버 클릭", Toast.LENGTH_SHORT).show();
-                    drawer.closeDrawer(GravityCompat.START);
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
                     break;
                 case R.id.nav_github:
                     Toast.makeText(context, "GitHub 클릭", Toast.LENGTH_SHORT).show();
-                    drawer.closeDrawer(GravityCompat.START);
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
             }
             return false;
         });
