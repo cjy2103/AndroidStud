f9a90028f6fc97ad8c1f387edd4fc27a296f406d
cjy2103 <cjy2103@naver.com>
2022-10-03 15:35:28 +0900

    위치권한 포스팅 완

--- for blog function pratice/JAVA/Permission/LocationPermission/app/src/main/java/com/example/locationpermission/MainActivity.java
+++ for blog function pratice/JAVA/Permission/LocationPermission/app/src/main/java/com/example/locationpermission/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.locationpermission;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.locationpermission.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private ActivityResultLauncher<String[]> locationPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts
+                            .RequestMultiplePermissions(), result -> {
+                        Boolean fineLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        Boolean coarseLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                        if (fineLocationGranted != null && fineLocationGranted) {
+                            // Precise location access granted.
+                            Toast.makeText(this, "정확환 위치 엑세스 권한 부여됨", Toast.LENGTH_SHORT).show();
+                        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                            // Only approximate location access granted.
+                            Toast.makeText(this, "대략적인 위치 액세스 권한 부여됨", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // No location access granted.
+                            Toast.makeText(this, "위치권한 부여되지 않음", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
+
+        locationPermissionRequest();
+
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void locationPermissionRequest(){
+        binding.button.setOnClickListener(v->{
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        });
     }
 }
