e1aebf0d972375ea43f71d0e5b06d73b25eea3b2
jy.choi <cjy2103@naver.com>
2022-10-04 17:28:52 +0900

    Hash 키 얻기

--- for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/MainActivity.java
@@ -5,27 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
+import com.example.kakaomap.databinding.ActivityMainBinding;
 import com.example.kakaomap.util.HashKey;
+
+import net.daum.mf.map.api.MapView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    MapView mapView = new MapView(this);
+    private ActivityMainBinding binding;
+
+    MapView mapView;
+    ViewGroup mapViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
 
         init();
 
     }
 
-    private void init(){
-        HashKey hashKey = new HashKey();
-        hashKey.getSignatures(this);
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 
+    private void init(){
+        mapView = new MapView(this);
+        mapViewContainer = binding.mapView;
+        mapViewContainer.addView(mapView);
     }
 
 
--- for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/util/HashKey.java
+++ for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/util/HashKey.java
@@ -7,28 +7,41 @@ import android.content.pm.Signature;
 import android.util.Base64;
 import android.util.Log;
 
+import com.example.kakaomap.BuildConfig;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashKey {
-    public void getSignatures(Context context){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
 
-        for (Signature signature : packageInfo.signatures) {
+    public void migrateSignatures(Context context) {
+        if (BuildConfig.DEBUG) {
+            PackageInfo packageInfo = null;
+
             try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+                packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNING_CERTIFICATES);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
             }
+
+            if (packageInfo == null)
+                Log.e("KeyHash", "KeyHash:null");
+
+            assert packageInfo != null;
+            Signature[] signatureInfo = packageInfo.signingInfo.getApkContentsSigners();
+
+            for (Signature signature : signatureInfo) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                } catch (NoSuchAlgorithmException e) {
+                    Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+                }
+
+            }
+
         }
     }
+
 }
