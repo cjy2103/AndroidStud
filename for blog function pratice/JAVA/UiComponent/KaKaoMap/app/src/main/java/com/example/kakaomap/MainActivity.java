4d164d3f03bb6b48d532f1502558f54c21a59842
jy.choi <cjy2103@naver.com>
2022-10-07 17:52:23 +0900

    카카오맵 마커달기 포스팅중

--- for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/KaKaoMap/app/src/main/java/com/example/kakaomap/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 
 import com.example.kakaomap.databinding.ActivityMainBinding;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        markerAdd();
     }
 
     private void viewBinding(){
@@ -37,6 +40,19 @@ public class MainActivity extends AppCompatActivity {
         mapView = new MapView(this);
         mapViewContainer = binding.mapView;
         mapViewContainer.addView(mapView);
+    }
+
+    private void markerAdd(){
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.1399, 129.0986), true);
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(35.1399, 129.0986);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("경성대학교");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker); // 지도위에 마커 추가
     }
 
 
