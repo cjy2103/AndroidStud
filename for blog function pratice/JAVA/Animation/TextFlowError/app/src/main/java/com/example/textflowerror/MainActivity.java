410e46e8ec3686eb12cd01ea0675dc07c8951897
jy.choi <cjy2103@naver.com>
2022-11-09 04:04:31 +0900

    바인딩 널 처리시 쓰레드 폐기 안함.

--- for blog function pratice/JAVA/Animation/TextFlowError/app/src/main/java/com/example/textflowerror/MainActivity.java
+++ for blog function pratice/JAVA/Animation/TextFlowError/app/src/main/java/com/example/textflowerror/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.textflowerror;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.textflowerror.databinding.ActivityMainBinding;
@@ -14,11 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
+
+        movePage();
     }
 
     private void viewBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
+
+    private void movePage(){
+        binding.btnMove.setOnClickListener(v->{
+            Intent intent = new Intent(this, SubActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
 
 }
---
+++ for blog function pratice/JAVA/Animation/TextFlowError/app/src/main/java/com/example/textflowerror/SubActivity.java
@@ -0,0 +1,38 @@
+package com.example.textflowerror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.example.textflowerror.databinding.ActivitySubBinding;
+
+public class SubActivity extends AppCompatActivity {
+
+    private ActivitySubBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewBinding();
+
+        textFlow();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    private void viewBinding(){
+        binding = ActivitySubBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void textFlow(){
+        binding.tvFlow.setSingleLine(true);
+        binding.tvFlow.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        binding.tvFlow.postDelayed(()-> binding.tvFlow.setSelected(true),1500);
+    }
+}
