8de48e84c1dfbeae87cddb434e37cd17be86bd4c
jy.choi <cjy2103@naver.com>
2022-12-01 11:57:58 +0900

    검색창 구현중

--- for blog function pratice/JAVA/RecyclerView/SearchView/app/src/main/java/com/example/searchview/activity/SearchActivity.java
+++ for blog function pratice/JAVA/RecyclerView/SearchView/app/src/main/java/com/example/searchview/activity/SearchActivity.java
@@ -5,20 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.searchview.databinding.ActivitySearchBinding;
+import com.example.searchview.model.SearchModel;
 
 public class SearchActivity extends AppCompatActivity {
 
     private ActivitySearchBinding binding;
+    private SearchModel searchModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
-
+        init();
     }
 
     private void viewBinding(){
         binding = ActivitySearchBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    private void init(){
+        searchModel = new SearchModel(this);
     }
 }
---
+++ for blog function pratice/JAVA/RecyclerView/SearchView/app/src/main/java/com/example/searchview/model/SearchModel.java
@@ -0,0 +1,12 @@
+package com.example.searchview.model;
+
+import com.example.searchview.activity.SearchActivity;
+
+public class SearchModel {
+
+    private SearchActivity searchActivity;
+
+    public SearchModel(SearchActivity searchActivity) {
+        this.searchActivity = searchActivity;
+    }
+}
