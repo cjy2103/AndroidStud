f576b5d1c4af59d46aeec5481f0dfd36eb4f97f0
jy.choi <cjy2103@naver.com>
2022-12-08 10:18:43 +0900

    DiffUtil 준비중

---
+++ for blog function pratice/JAVA/RecyclerView/DiffUtil/app/src/main/java/com/example/diffutil/dto/ListItem.java
@@ -0,0 +1,31 @@
+package com.example.diffutil.dto;
+
+public class ListItem {
+    private String title;
+    private String describe;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
