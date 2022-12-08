1c832bc73cfc8a2369627138f606ff6f012b9dff
jy.choi <cjy2103@naver.com>
2022-12-08 11:15:03 +0900

    DiffUitl 준비중

--- for blog function pratice/JAVA/RecyclerView/DiffUtil/app/src/main/java/com/example/diffutil/MainActivity.java
+++ for blog function pratice/JAVA/RecyclerView/DiffUtil/app/src/main/java/com/example/diffutil/MainActivity.java
@@ -3,21 +3,42 @@ package com.example.diffutil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.diffutil.adapter.ListItemAdapter;
 import com.example.diffutil.databinding.ActivityMainBinding;
+import com.example.diffutil.model.MainModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private ActivityMainBinding binding;
+    private MainModel mainModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewBinding();
+        init();
+        listAdd();
     }
 
     private void viewBinding(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
+
+    private void init(){
+        mainModel = new MainModel(this,this);
+    }
+
+    private void listAdd(){
+        binding.btnAdd.setOnClickListener(v->{
+            mainModel.listAdd();
+        });
+    }
+
+    public void setRecycler(ListItemAdapter adapter){
+        binding.recyclerList.setAdapter(adapter);
+    }
+
 }
---
+++ for blog function pratice/JAVA/RecyclerView/DiffUtil/app/src/main/java/com/example/diffutil/adapter/ListItemAdapter.java
@@ -0,0 +1,67 @@
+package com.example.diffutil.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.diffutil.R;
+import com.example.diffutil.databinding.RecyclerViewListBinding;
+import com.example.diffutil.dto.ListItem;
+
+import java.util.ArrayList;
+
+public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder>{
+
+    private Context context;
+    private ArrayList<ListItem> list;
+
+    public ListItemAdapter(Context context, ArrayList<ListItem> list) {
+        this.context = context;
+        this.list    = list;
+    }
+
+    public void setList(ArrayList<ListItem> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_view_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Uri uri = Uri.parse("android.resource://" + context.getPackageName() +
+                list.get(position).getUrl());
+        Glide.with(context).load(uri).into(holder.binding.ivArt);
+        holder.binding.tvTitle.setText(list.get(position).getTitle());
+        holder.binding.tvDescribe.setText(list.get(position).getDescribe());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private RecyclerViewListBinding binding;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            viewBinding();
+        }
+
+        private void viewBinding(){
+            binding = RecyclerViewListBinding.bind(itemView);
+        }
+    }
+}
