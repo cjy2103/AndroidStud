eabf716183295f0745275a09cb1118bda693415e
jy.choi <cjy2103@naver.com>
2022-12-12 15:00:29 +0900

    AAC 뷰모델 준비중

--- for blog function pratice/JAVA/DesignPattern/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.java
+++ for blog function pratice/JAVA/DesignPattern/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.java
@@ -1,16 +1,67 @@
 package com.example.viewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.example.viewmodel.databinding.ActivityMainBinding;
+import com.example.viewmodel.model.MainModel;
+import com.example.viewmodel.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-        
+    private ActivityMainBinding binding;
+
+    private MainModel mainModel;
+    private MainViewModel mainViewModel;
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
+        init();
+        change();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!mainViewModel.initLoad){
+            Glide.with(this).load(mainViewModel.imagePath).into(binding.ivImage);
+            binding.tvName.setText(mainViewModel.title);
+            num = mainViewModel.num;
+        }
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    private void init(){
+        mainModel = new MainModel(this);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    }
+
+    private void change(){
+        binding.btnChange.setOnClickListener(v->{
+            num++;
+            Uri image = mainModel.getImagePath(num);
+            String title = mainModel.getTitle(num);
+            Glide.with(this).load(image).into(binding.ivImage);
+            binding.tvName.setText(title);
+            dataSave(image, title);
+        });
+    }
+
+    private void dataSave(Uri image, String title){
+        mainViewModel.imagePath = image;
+        mainViewModel.title = title;
+        mainViewModel.initLoad = false;
+        mainViewModel.num = num;
     }
 }
---
+++ for blog function pratice/JAVA/DesignPattern/ViewModel/app/src/main/java/com/example/viewmodel/viewmodel/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.example.viewmodel.viewmodel;
+import android.net.Uri;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public boolean initLoad = true;
+    public Uri imagePath;
+    public String title;
+    public int num = 0;
+}
