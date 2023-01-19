cdcde62b4b61d0fdef0e62a1e617520f3b02abeb
jy.choi <cjy2103@naver.com>
2023-01-19 10:34:34 +0900

    ToolTipText

--- for blog function pratice/JAVA/UiComponent/VIewToolTipText/app/src/main/java/com/example/tooptip/MainActivity.java
+++ for blog function pratice/JAVA/UiComponent/VIewToolTipText/app/src/main/java/com/example/tooptip/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.tooptip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import com.example.tooptip.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.ivTabTam.setTooltipText("탬탬버린");
+        }
+    }
+
+    private void viewBinding(){
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
