2c3d25a20ad5977a449d53d74d7de8926fe364d6
jy.choi <cjy2103@naver.com>
2023-02-03 10:16:05 +0900

    MVVM으로 RecyclerView 만들기

---
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerVie(MVVM)/app/src/main/java/com/example/recyclerviewmvvm/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.recyclerviewmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.recyclerviewmvvm.databinding.ActivityMainBinding;
+import com.example.recyclerviewmvvm.vm.MainViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel = new MainViewModel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dataBinding();
+    }
+
+    private void dataBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(this);
+    }
+}
---
+++ for blog function pratice/JAVA/RecyclerView/CustomRecyclerVie(MVVM)/app/src/main/java/com/example/recyclerviewmvvm/vm/MainViewModel.java
@@ -0,0 +1,5 @@
+package com.example.recyclerviewmvvm.vm;
+
+public class MainViewModel {
+
+}
