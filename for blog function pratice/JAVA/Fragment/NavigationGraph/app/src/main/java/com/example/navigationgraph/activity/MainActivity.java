3af8fca2d33b4de32ffec02df7fb89ca975514ed
jy.choi <cjy2103@naver.com>
2023-04-19 14:11:38 +0900

    내비게이션 그래프 포스팅 완

--- for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/activity/MainActivity.java
+++ for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.navigationgraph.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
@@ -7,17 +8,22 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.navigationgraph.R;
 import com.example.navigationgraph.databinding.ActivityMainBinding;
 import com.example.navigationgraph.util.SystemUtil;
 import com.example.navigationgraph.vm.MainViewModel;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MainViewModel viewModel = new MainViewModel();
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         dataBinding();
 
         init();
+
+//        clickBottomTab();
+//
+//        observeViewModel();
     }
 
     private void dataBinding(){
@@ -42,8 +52,36 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationUI.setupWithNavController(binding.bottomNav, navController);
 
+//        menu = binding.bottomNav.getMenu();
+//        menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax_fail);
+
         SystemUtil.sofNavigationBarHide(getWindow());
         SystemUtil.statusbarSetting(getWindow(),this,binding.mainConstraint);
     }
+
+
+//    @SuppressLint("NonConstantResourceId")
+//    private void clickBottomTab(){
+//        binding.bottomNav.setOnItemSelectedListener(item -> {
+//            menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax);
+//            menu.findItem(R.id.momoi_fragment).setIcon(R.drawable.iv_momoi);
+//            menu.findItem(R.id.midori_fragment).setIcon(R.drawable.iv_midori);
+//            switch (item.getItemId()){
+//                case R.id.djmax_fragment ->  viewModel.setItemIconRes(R.id.djmax_fragment, R.drawable.iv_djmax_fail);
+//                case R.id.momoi_fragment ->  viewModel.setItemIconRes(R.id.momoi_fragment, R.drawable.iv_alice);
+//                case R.id.midori_fragment ->  viewModel.setItemIconRes(R.id.midori_fragment, R.drawable.iv_yuse);
+//            }
+//            return false;
+//        });
+//    }
+//
+//    private void observeViewModel(){
+//        viewModel.getItemIconRes().observe(this, itemIconRes -> {
+//            MenuItem item = binding.bottomNav.getMenu().findItem(itemIconRes.getItemId());
+//            if (item != null) {
+//                item.setIcon(itemIconRes.getIconResId());
+//            }
+//        });
+//    }
 
 }
--- for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/vm/MainViewModel.java
+++ for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/vm/MainViewModel.java
@@ -1,8 +1,18 @@
 package com.example.navigationgraph.vm;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.navigationgraph.data.ItemIconRes;
+
 public class MainViewModel {
 
-    public void bottomNavClick(){
+    private MutableLiveData<ItemIconRes> itemIconRes = new MutableLiveData<>();
 
+    public MutableLiveData<ItemIconRes> getItemIconRes() {
+        return itemIconRes;
+    }
+
+    public void setItemIconRes(int itemId, int iconResId){
+        itemIconRes.setValue(new ItemIconRes(itemId, iconResId));
     }
 }
