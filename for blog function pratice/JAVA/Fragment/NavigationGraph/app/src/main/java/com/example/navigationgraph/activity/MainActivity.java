86a1c03ea67c1467d589d670fd51b49ece8bc090
jy.choi <cjy2103@naver.com>
2023-04-20 14:59:08 +0900

    아이콘 변경 기능 포스팅 완

--- for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/activity/MainActivity.java
+++ for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/activity/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MainViewModel viewModel = new MainViewModel();
+    private NavController navController;
     private Menu menu;
 
     @Override
@@ -32,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
-//        clickBottomTab();
-//
-//        observeViewModel();
+        iconChange();
     }
 
     private void dataBinding(){
@@ -48,40 +47,20 @@ public class MainActivity extends AppCompatActivity {
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_frame);
         assert navHostFragment != null;
-        NavController navController = navHostFragment.getNavController();
+        navController = navHostFragment.getNavController();
 
         NavigationUI.setupWithNavController(binding.bottomNav, navController);
 
-//        menu = binding.bottomNav.getMenu();
-//        menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax_fail);
+        menu = binding.bottomNav.getMenu();
 
         SystemUtil.sofNavigationBarHide(getWindow());
         SystemUtil.statusbarSetting(getWindow(),this,binding.mainConstraint);
     }
 
-
-//    @SuppressLint("NonConstantResourceId")
-//    private void clickBottomTab(){
-//        binding.bottomNav.setOnItemSelectedListener(item -> {
-//            menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax);
-//            menu.findItem(R.id.momoi_fragment).setIcon(R.drawable.iv_momoi);
-//            menu.findItem(R.id.midori_fragment).setIcon(R.drawable.iv_midori);
-//            switch (item.getItemId()){
-//                case R.id.djmax_fragment ->  viewModel.setItemIconRes(R.id.djmax_fragment, R.drawable.iv_djmax_fail);
-//                case R.id.momoi_fragment ->  viewModel.setItemIconRes(R.id.momoi_fragment, R.drawable.iv_alice);
-//                case R.id.midori_fragment ->  viewModel.setItemIconRes(R.id.midori_fragment, R.drawable.iv_yuse);
-//            }
-//            return false;
-//        });
-//    }
-//
-//    private void observeViewModel(){
-//        viewModel.getItemIconRes().observe(this, itemIconRes -> {
-//            MenuItem item = binding.bottomNav.getMenu().findItem(itemIconRes.getItemId());
-//            if (item != null) {
-//                item.setIcon(itemIconRes.getIconResId());
-//            }
-//        });
-//    }
+    private void iconChange(){
+        navController.addOnDestinationChangedListener(((controller, destination, bundle) -> {
+            viewModel.updateBottomNavIcon(menu, destination);
+        }));
+    }
 
 }
--- for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/vm/MainViewModel.java
+++ for blog function pratice/JAVA/Fragment/NavigationGraph/app/src/main/java/com/example/navigationgraph/vm/MainViewModel.java
@@ -1,18 +1,23 @@
 package com.example.navigationgraph.vm;
 
-import androidx.lifecycle.MutableLiveData;
+import android.annotation.SuppressLint;
+import android.view.Menu;
 
-import com.example.navigationgraph.data.ItemIconRes;
+import androidx.navigation.NavDestination;
+
+import com.example.navigationgraph.R;
 
 public class MainViewModel {
 
-    private MutableLiveData<ItemIconRes> itemIconRes = new MutableLiveData<>();
-
-    public MutableLiveData<ItemIconRes> getItemIconRes() {
-        return itemIconRes;
-    }
-
-    public void setItemIconRes(int itemId, int iconResId){
-        itemIconRes.setValue(new ItemIconRes(itemId, iconResId));
+    @SuppressLint("NonConstantResourceId")
+    public void updateBottomNavIcon(Menu menu, NavDestination destination){
+        menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax);
+        menu.findItem(R.id.momoi_fragment).setIcon(R.drawable.iv_momoi);
+        menu.findItem(R.id.midori_fragment).setIcon(R.drawable.iv_midori);
+        switch (destination.getId()){
+            case R.id.djmax_fragment -> menu.findItem(R.id.djmax_fragment).setIcon(R.drawable.iv_djmax_fail);
+            case R.id.momoi_fragment ->  menu.findItem(R.id.momoi_fragment).setIcon(R.drawable.iv_alice);
+            case R.id.midori_fragment -> menu.findItem(R.id.midori_fragment).setIcon(R.drawable.iv_yuse);
+        }
     }
 }
